49db115b447a67757edcab3efb4158c00bd0877f
Tayo Moore <tayo.moore@gmail.com>
2023-08-20 22:40:46 +0100

    trying to sort linting

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
 <script>
         // Fetch the temperature from the server
+    fetch('/temperature')
+            .then(response => response.json())
+            .then(data => {
+        document.getElementById('temperature').textContent = data.temperature;
+            });
+
+    // Function to fetch and display the temperature
+    function updateTemperature() {
         fetch('/temperature')
             .then(response => response.json())
             .then(data => {
                 document.getElementById('temperature').textContent = data.temperature;
+            })
+            .catch(error => {
+                console.error('Error fetching temperature:', error);
+                document.getElementById('temperature').textContent = 'Error fetching temperature.';
             });
-
-        // Function to fetch and display the temperature
-        function updateTemperature() {
-            fetch('/temperature')
-                .then(response => response.json())
-                .then(data => {
-                    document.getElementById('temperature').textContent = data.temperature;
-                })
-                .catch(error => {
-                    console.error('Error fetching temperature:', error);
-                    document.getElementById('temperature').textContent = 'Error fetching temperature.';
-                });
         }
 
-        // Call the function initially to set the temperature when the page loads
-        updateTemperature();
+    // Call the function initially to set the temperature when the page loads
+    updateTemperature();
 
-        // Attach the function to the button's click event
-        document.getElementById('refreshTemperature').addEventListener('click', updateTemperature);
-    </script>
+    // Attach the function to the button's click event
+    document.getElementById('refreshTemperature').addEventListener('click', updateTemperature);
+</script>
