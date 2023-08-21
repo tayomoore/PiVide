20712ce6d68e8d4ad3f74ed2c7328b9b22f567f3
Tayo Moore <tayo.moore@gmail.com>
2023-08-21 18:50:23 +0100

    adding eslint

--- public/main.js
+++ public/main.js
@@ -1,26 +1,26 @@
-        // Fetch the temperature from the server
-    fetch('/temperature')
-            .then(response => response.json())
-            .then(data => {
-        document.getElementById('temperature').textContent = data.temperature;
-            });
+// Fetch the temperature from the server
+fetch("/temperature")
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("temperature").textContent = data.temperature;
+    });
 
-    // Function to fetch and display the temperature
-    function updateTemperature() {
-        fetch('/temperature')
-            .then(response => response.json())
-            .then(data => {
-                let formattedTemperature = parseFloat(data.temperature).toFixed(2);
-                document.getElementById('temperature').textContent = formattedTemperature;
-            })
-            .catch(error => {
-                console.error('Error fetching temperature:', error);
-                document.getElementById('temperature').textContent = 'Error fetching temperature.';
-            });
-        }
+// Function to fetch and display the temperature
+function updateTemperature() {
+    fetch("/temperature")
+        .then(response => response.json())
+        .then(data => {
+            let formattedTemperature = parseFloat(data.temperature).toFixed(1);
+            document.getElementById("temperature").textContent = formattedTemperature;
+        })
+        .catch(error => {
+            console.error("Error fetching temperature:", error);
+            document.getElementById("temperature").textContent = "Error fetching temperature.";
+        });
+}
 
-    // Call the function initially to set the temperature when the page loads
-    updateTemperature();
+// Call the function initially to set the temperature when the page loads
+updateTemperature();
 
-    // Attach the function to the button's click event
-    document.getElementById('refreshTemperature').addEventListener('click', updateTemperature);
+// Attach the function to the button's click event
+document.getElementById("refreshTemperature").addEventListener("click", updateTemperature);
--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const ds18b20 = require('ds18b20');
-require('dotenv').config();
+const express = require("express");
+const ds18b20 = require("ds18b20");
+require("dotenv").config();
 const app = express();
 const PORT = 3000;
 
 // This will serve your static files
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/temperature', (req, res) => {
+app.get("/temperature", (req, res) => {
     // Your sensor's ID
     const sensorId = process.env.DS18B20_SENSOR_ID;
     ds18b20.temperature(sensorId, (err, value) => {
         if (err) {
             console.error(`Error reading temperature: ${err}`);
-            return res.status(500).send('Error reading temperature.');
+            return res.status(500).send("Error reading temperature.");
         }
         res.json({ temperature: value.toString() });
     });
